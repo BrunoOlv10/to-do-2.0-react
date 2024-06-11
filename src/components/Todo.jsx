243efda4b4396a8cdf96b5a0cb00e5d94d62159f
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2024-06-10 23:12:50 -0300

    add updates

---
+++ src/components/Todo.jsx
@@ -0,0 +1,14 @@
+const Todo = ({ todo }) => {
+  return (
+    <div className="todo">
+    <div className="content">
+      <p>{todo.text}</p>
+      <p className="category">({todo.category})</p>
+    </div>
+      <button className="complete">Completar</button>
+      <button className="remove">x</button>
+  </div>
+  )
+}
+
+export default Todo
